0257c083e2ba2a823ffbb13451756eb61cdc0127
Skylar Werner <skylar_werner@yahoo.com>
2019-12-08 15:59:11 +0300

    Vertices display correctly

--- CG2/MainWindow.xaml.cs
+++ CG2/MainWindow.xaml.cs
@@ -25,71 +25,68 @@ namespace CG2
             InitializeComponent();
 
             Canvas paper = Paper;
-            //const int nPoints = 8;
-            //const int nEdges = 12;
-            Polygon line = PolygonLine();
+            const int nPoints = 8;
+            const int nEdges = 12;
+            Line line = new Line();
 
-            //// Build a table of vertices
-            //double[,] vtable = new double[nPoints, 3]
-            //{
-            //    { -0.5, -0.5, 2.5}, { -0.5, 0.5, 2.5},
-            //    { 0.5, 0.5 ,2.5}, {0.5, -0.5, 2.5},
-            //    { -0.5, -0.5, 3.5}, { -0.5, 0.5, 3.5},
-            //    { 0.5, 0.5, 3.5}, {0.5, -0.5, 3.5},
-            //};
-
-            //// Build a table of edges
-            //int[,] etable = new int[nEdges, 2]
-            //{
-            //    {0,1}, {1,2}, {2,3}, {3,0}, {0,4}, {1,5},
-            //    {2,6}, {3,7}, {4,5}, {5,6}, {6,7}, {7,4}
-            //};
-
-            //double xmin = -0.5, xmax = 0.5;
-            //double ymin = -0.5, ymax = 0.5;
-
-            //Point[] pictureVertices = new Point[nPoints];
-
-            //double scale = 100;
-            //for(int i = 0; i < nPoints; i++)
-            //{
-            //    double x = vtable[i, 0];
-            //    double y = vtable[i, 2];
-            //    double z = vtable[i, 3];
-            //    double xprime = x / z;
-            //    double yprime = y / z;
-
-            //    pictureVertices[i].X = scale *
-            //        (1 - (xprime - xmin) / (xmax - xmin));
-            //    pictureVertices[i].Y = scale *
-            //        (1 - (yprime - ymin) / (ymax - ymin));
-            //    line.Points.Add(pictureVertices[i].X,
-            //        pictureVertices[i].Y);
-            //    //gp.Children.Add(new Point(pictureVertices[i]);
-            //}
-
-            PointCollection points = new PointCollection();
-            for(float x=0; x < 50; x++)
+            // Build a table of vertices
+            double[,] vtable = new double[nPoints, 3]
             {
-                Point p = new Point(x, Math.Sin(x));
-                points.Add(p);
+                { -0.5, -0.5, 2.5}, { -0.5, 0.5, 2.5},
+                { 0.5, 0.5 ,2.5}, {0.5, -0.5, 2.5},
+                { -0.5, -0.5, 3.5}, { -0.5, 0.5, 3.5},
+                { 0.5, 0.5, 3.5}, {0.5, -0.5, 3.5},
+            };
+
+            // Build a table of edges
+            int[,] etable = new int[nEdges, 2]
+            {
+                {0,1}, {1,2}, {2,3}, {3,0}, {0,4}, {1,5},
+                {2,6}, {3,7}, {4,5}, {5,6}, {6,7}, {7,4}
+            };
+
+            double xmin = -0.5, xmax = 0.5;
+            double ymin = -0.5, ymax = 0.5;
+
+            Point[] pictureVertices = new Point[nPoints];
+            Ellipse dot = new Ellipse();
+            int scale = 40;
+            
+            for (int i = 0; i < nPoints; i++)
+            {
+                double x = vtable[i, 0];
+                double y = vtable[i, 1];
+                double z = vtable[i, 2];
+                double xprime = x / z;
+                double yprime = y / z;
+
+                x = scale *
+                    (1 - (xprime - xmin) / (xmax - xmin));
+                y = scale *
+                    (1 - (yprime - ymin) / (ymax - ymin));
+
+                dot = Dot(x, y);
+
+                paper.Children.Add(dot);
             }
-
-            line.Points = points;
-            paper.Children.Add(line);
-
+            for(int i=0; i < nEdges; i++)
+            {
+                int n1 = etable[i, 0];
+                int n2 = etable[i, 1];
+            }
         }
 
-        private Polygon PolygonLine()
+        private Ellipse Dot(double x, double y)
         {
-            Polygon polygonLine = new Polygon();
-            polygonLine.Stroke = Brushes.Black;
-            polygonLine.Fill = Brushes.LightSeaGreen;
-            polygonLine.StrokeThickness = 0.2;
-            polygonLine.HorizontalAlignment = HorizontalAlignment.Left;
-            polygonLine.VerticalAlignment = VerticalAlignment.Center;
+            Ellipse currentDot = new Ellipse();
+            currentDot.Stroke = new SolidColorBrush(Colors.Black);
+            currentDot.StrokeThickness = .1;
+            currentDot.Height = 1;
+            currentDot.Width = 1;
+            currentDot.Fill = new SolidColorBrush(Colors.Black);
+            currentDot.Margin = new Thickness(x, y, 0, 0);
 
-            return polygonLine;
+            return currentDot;
         }
     }
 }
